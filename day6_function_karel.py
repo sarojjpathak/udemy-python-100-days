1a8fb6a87bffe0b0f232ec61a41a7b95b312f23e
sarojjpathk <pathaksaroj695@gmail.com>
2026-06-20 09:47:58 +0545

    completed day 6 with a lot of practices i reeborgs

---
+++ day6_function_karel.py
@@ -0,0 +1,75 @@
+# defining a function 
+def sum(para1,para2):
+    print(f"sum of {para1} and {para2} is {para1+para2}")
+
+
+arg1 = 23
+arg2 = 45
+#know calling a function
+sum(arg1,arg2)
+
+
+
+
+#i have dont some practices in reeborg to understand the function concept more clearly  here are some code to solve that problem
+# logoReeborg's World
+
+# Hurdle 1 ✓🤖
+
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+# def cross_obstacle():
+#     move()
+#     turn_left()
+#     move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     move()
+#     turn_left()
+# for i in range(0,6):
+#     cross_obstacle()
+# ​
+# ​this was the code i wrote to solve the hurdle 1
+
+
+# NOTE : in python indentation is very important
+# def check_student_marks(marks):
+# │
+# ├── if marks >= 40:  # as if is inside a function so its not in same level as main func
+# │   │
+# │   ├── print("Pass")
+# │   │
+# │   ├── if marks >= 80:
+# │   │   └── print("Grade: A")
+# │   │
+# │   ├── elif marks >= 60:
+# │   │   └── print("Grade: B")
+# │   │
+# │   └── else:
+# │       └── print("Grade: C")
+# │
+# └── else:
+#     └── print("Fail")
+li =[1,2,3,4,5]
+def greet():
+    if 3 in li:
+        print("wow")
+# i prefer tab over space  for indentation :) 1 tab = 4 space so i prefer 1 tab over 4 space
+
+
+
+#while loop
+# while something is true:
+#      do this 
+#      do this 
+#      ------
+# in FOR LOOP = do something for each item  it has a limit
+# while do same thing repetedly until true condition 
+
+
+
+#hurdle races  where we have different function like 
+#move(),turn_left() ,
