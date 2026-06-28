86c0f0fe9e2f6994bee07cb6f965300d031c167d
sarojjpathk <pathaksaroj695@gmail.com>
2026-06-28 22:10:42 +0545

    minor change in black jack game

--- day11_blackjack_game.py
+++ day11_blackjack_game.py
@@ -51,35 +51,48 @@ while ((True if play == "y" else False) and status) or new :
     print(f"deales :[{dealer[0]}, H ] player : {player}   player sum = {sum(player)}")
     
     while True:
-        if dealersum == 21:
-            displayCard(dealer,player)
-            print("you loose dealer has a black jack ")
-            break
-        elif playersum == 21:
-            displayCard(dealer,player)
-            print("you won Black jack")
-            break
-        elif playersum > 21:
-            if 11 in player:
-                player.remove(11)
-                player.append(1)
+        playersum = sum(player)
+        dealersum = sum(dealer)
+        if len(player) == 2 and len(dealer) == 2:
+            if dealersum == 21:
                 displayCard(dealer,player)
-                playersum = sum(player)
-            else:  
-                displayCard(dealer,player)
-                print("you loose")
-                status = False
+                print("you loose DEALER BLACK JACK ")
                 break
+            
+            elif playersum == 21:
+                displayCard(dealer,player)
+                print("you won BLACK JACK")
+                break
+        else:
+            if dealersum == 21:
+                    displayCard(dealer,player)
+                    print("you loose dealer win ")
+                    break
+            elif playersum == 21:
+                    displayCard(dealer,player)
+                    print("you won")
+                    break
+        if playersum > 21:
+                if 11 in player:
+                    player.remove(11)
+                    player.append(1)
+                    displayCard(dealer,player)
+                    playersum = sum(player)
+                else:  
+                    displayCard(dealer,player)
+                    print("you loose")
+                    status = False
+                    break
         elif playersum <21:
-            hitorstand = input("do you want to hit or stand(hit/stand)")
-            if hitorstand == "hit":
-                player.append(random.choice(card))
-                playersum = sum(player)
-                print(f"YOU HIT SO new set of card is {player}")
-                print(f"deales :[{dealer[1]}, H ] player : {player}   player sum = {sum(player)}")
-            elif hitorstand == "stand":
-                status = stand(dealer,player,card)
-                break
+                hitorstand = input("do you want to hit or stand(hit/stand)")
+                if hitorstand == "hit":
+                    player.append(random.choice(card))
+                    playersum = sum(player)
+                    print(f"YOU HIT SO new set of card is {player}")
+                    print(f"deales :[{dealer[1]}, H ] player : {player}   player sum = {sum(player)}")
+                elif hitorstand == "stand":
+                    status = stand(dealer,player,card)
+                    break
 
     newgame = input("NEW GAME (y/n)")
     if newgame == "y":
