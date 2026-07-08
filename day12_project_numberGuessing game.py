8acef91f9bbc00e1415001b37a06921baa170c87
sarojjpathk <pathaksaroj695@gmail.com>
2026-07-08 22:34:02 +0545

    this is verssion 2 of this game where user cant give same number again and again as there is no point for doing that

--- day12_project_numberGuessing game.py
+++ day12_project_numberGuessing game.py
@@ -26,22 +26,29 @@ ___________                        _________                      __  .__    .__
 
 print("_______WELCOME TO NUMBER GUESSING GAME_________")
 
-level = input("Choose a difficulty Level (hard/easy)")
+
 attempt = set_difficulties()
 number_to_guess = random.randint(1,100)
-print(number_to_guess)
+list_of_guess_num = []
+# print(number_to_guess)
 while attempt != 0:
     guessed_number = int(input("Guess a number from 1 t0 100 :"))
-    if guessed_number == number_to_guess:
-        print("you have won this game")
-        break
-    elif guessed_number < number_to_guess:
-        print("too low ")
-        attempt -= 1
-    elif guessed_number > number_to_guess:
-        print("too high")
-        attempt -= 1
-    print(f"Attempt Available : {attempt}")
+    if guessed_number in list_of_guess_num:
+        print("you have already guessed this number try another number")
+    else:
+        list_of_guess_num.append(guessed_number)
+
+        # print(list_of_guess_num)
+        if guessed_number == number_to_guess:
+            print("you have won this game")
+            break
+        elif guessed_number < number_to_guess:
+            print("too low ")
+            attempt -= 1
+        elif guessed_number > number_to_guess:
+            print("too high")
+            attempt -= 1
+        print(f"Attempt Available : {attempt}")
 if attempt == 0:
     print("you loose this game")
 
